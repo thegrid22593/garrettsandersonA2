d18f7e05054c59cc7575ef188b8fbb1058248ee2
Garrett Sanderson <garrett.sanderson@gmail.com>
2017-02-17 01:17:29 -0800

    adds footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  public copyrightDate;
   constructor() { }
 
   ngOnInit() {
+    this.copyrightDate = new Date();
+    this.copyrightDate = this.copyrightDate.getFullYear();
   }
 
 }
